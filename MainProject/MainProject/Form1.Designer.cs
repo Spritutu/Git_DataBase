39a337ee89e9c6d329c04d90d86ffb9d35576eb4
jameschnbass <jameschn.bass@gmail.com>
2016-01-12 15:37:08 +0800

    更改名稱

--- MainProject/MainProject/Form1.Designer.cs
+++ MainProject/MainProject/Form1.Designer.cs
@@ -28,35 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.components = new System.ComponentModel.Container();
+            this.MainWindow = new HalconDotNet.HWindowControl();
+            this.ProcedureTable = new System.Windows.Forms.DataGridView();
+            this.MainWindowObjectTable = new System.Windows.Forms.DataGridView();
+            this.PrcedureStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.MainWindowStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.插入程序ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.刪除程序ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新增程序ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.ProcedureTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainWindowObjectTable)).BeginInit();
+            this.PrcedureStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // hWindowControl1
+            // MainWindow
             // 
-            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
-            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
-            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
-            this.hWindowControl1.Location = new System.Drawing.Point(12, 41);
-            this.hWindowControl1.Name = "hWindowControl1";
-            this.hWindowControl1.Size = new System.Drawing.Size(965, 616);
-            this.hWindowControl1.TabIndex = 0;
-            this.hWindowControl1.WindowSize = new System.Drawing.Size(965, 616);
+            this.MainWindow.BackColor = System.Drawing.Color.Black;
+            this.MainWindow.BorderColor = System.Drawing.Color.Black;
+            this.MainWindow.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.MainWindow.Location = new System.Drawing.Point(12, 41);
+            this.MainWindow.Name = "MainWindow";
+            this.MainWindow.Size = new System.Drawing.Size(801, 616);
+            this.MainWindow.TabIndex = 0;
+            this.MainWindow.WindowSize = new System.Drawing.Size(801, 616);
+            // 
+            // ProcedureTable
+            // 
+            this.ProcedureTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProcedureTable.Location = new System.Drawing.Point(1021, 41);
+            this.ProcedureTable.Name = "ProcedureTable";
+            this.ProcedureTable.RowTemplate.Height = 24;
+            this.ProcedureTable.Size = new System.Drawing.Size(386, 616);
+            this.ProcedureTable.TabIndex = 1;
+            // 
+            // MainWindowObjectTable
+            // 
+            this.MainWindowObjectTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MainWindowObjectTable.Location = new System.Drawing.Point(819, 41);
+            this.MainWindowObjectTable.Name = "MainWindowObjectTable";
+            this.MainWindowObjectTable.RowTemplate.Height = 24;
+            this.MainWindowObjectTable.Size = new System.Drawing.Size(196, 616);
+            this.MainWindowObjectTable.TabIndex = 2;
+            // 
+            // PrcedureStrip
+            // 
+            this.PrcedureStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新增程序ToolStripMenuItem,
+            this.插入程序ToolStripMenuItem,
+            this.刪除程序ToolStripMenuItem});
+            this.PrcedureStrip.Name = "PrcedureStrip";
+            this.PrcedureStrip.Size = new System.Drawing.Size(125, 70);
+            // 
+            // MainWindowStatusStrip
+            // 
+            this.MainWindowStatusStrip.Location = new System.Drawing.Point(0, 713);
+            this.MainWindowStatusStrip.Name = "MainWindowStatusStrip";
+            this.MainWindowStatusStrip.Size = new System.Drawing.Size(1419, 22);
+            this.MainWindowStatusStrip.TabIndex = 4;
+            this.MainWindowStatusStrip.Text = "MainWindowStatusStrip";
+            // 
+            // 插入程序ToolStripMenuItem
+            // 
+            this.插入程序ToolStripMenuItem.Name = "插入程序ToolStripMenuItem";
+            this.插入程序ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.插入程序ToolStripMenuItem.Text = "插入程序";
+            // 
+            // 刪除程序ToolStripMenuItem
+            // 
+            this.刪除程序ToolStripMenuItem.Name = "刪除程序ToolStripMenuItem";
+            this.刪除程序ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.刪除程序ToolStripMenuItem.Text = "刪除程序";
+            // 
+            // 新增程序ToolStripMenuItem
+            // 
+            this.新增程序ToolStripMenuItem.Name = "新增程序ToolStripMenuItem";
+            this.新增程序ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.新增程序ToolStripMenuItem.Text = "新增程序";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1419, 735);
-            this.Controls.Add(this.hWindowControl1);
+            this.Controls.Add(this.MainWindowStatusStrip);
+            this.Controls.Add(this.MainWindowObjectTable);
+            this.Controls.Add(this.ProcedureTable);
+            this.Controls.Add(this.MainWindow);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ProcedureTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainWindowObjectTable)).EndInit();
+            this.PrcedureStrip.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private HalconDotNet.HWindowControl hWindowControl1;
+        private HalconDotNet.HWindowControl MainWindow;
+        private System.Windows.Forms.DataGridView ProcedureTable;
+        private System.Windows.Forms.DataGridView MainWindowObjectTable;
+        private System.Windows.Forms.ContextMenuStrip PrcedureStrip;
+        private System.Windows.Forms.StatusStrip MainWindowStatusStrip;
+        private System.Windows.Forms.ToolStripMenuItem 新增程序ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 插入程序ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 刪除程序ToolStripMenuItem;
     }
 }
 
