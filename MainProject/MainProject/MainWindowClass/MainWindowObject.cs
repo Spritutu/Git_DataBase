b61bc35cc0cfaeb33be4d0c113356a577993d935
jameschnbass <jameschn.bass@gmail.com>
2016-01-12 15:42:51 +0800

    新增MainWindowObject class

---
+++ MainProject/MainProject/MainWindowClass/MainWindowObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using HalconDotNet;
+
+namespace MainProject
+{
+    public class MainWindowObject
+    {
+        public static ArrayList Object = new ArrayList();
+    }
+}
